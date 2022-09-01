92d39d8c2d605fc59f0e7a4bc3ba7a7fdb884bea
Katie Baeten <katherinebaeten@gmail.com>
2022-08-31 20:26:16 -0500

    advice app process

---
+++ affirmation-station/src/App.js
@@ -0,0 +1,46 @@
+//Here we create a class-based component
+//App Component is the main component in React which acts as a container for all other components
+//Inside the App Component we will have a State, a global object that contains all of the most important things)
+import React from "react";
+import axios from "axios";
+
+import "./App.css";
+
+class App extends React.Component {
+	state = { advice: "" };
+
+	componentDidMount() {
+		this.fetchAdvice();
+	}
+
+	fetchAdvice = () => {
+		axios
+			.get("https://api.adviceslip.com/advice")
+			.then((response) => {
+				const { advice } = response.data.slip;
+
+				this.setState({ advice });
+			})
+
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+	render() {
+		const { advice } = this.state;
+
+		return (
+			<div className='app'>
+				<div className='card'>
+					<h1 className='heading'>{this.state.advice}</h1>
+					<button className='button' onClick={this.fetchAdvice}>
+						<span>Give Me Advice</span>
+					</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
